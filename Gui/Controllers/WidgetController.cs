18a01bc2071aa79402d12a4aeb863fc45ed6b310
Mikael Chudinov <mikael@chudinov.net>
2016-02-24 00:59:30 +0100

    Update packages. Add view and Index controller.

---
+++ Gui/Controllers/WidgetController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Repositories;
+using UseCases;
+
+namespace Gui.Controllers
+{
+    public class WidgetController : Controller
+    {
+        private readonly IWidgetRepository _repo;
+        private readonly IWidgetCase _case;
+
+        public WidgetController()
+        {
+            _repo = new WidgetRepository();
+            _case = new WidgetCase();
+        }
+
+        // GET: Widget
+        public ActionResult Index()
+        {
+            return View(_repo.GetAllWidgets());
+        }
+    }
+}
